454e940f862b8b50e81ee9511a31e8e0681407c5
EclipseManic <eclipseg12345@gmail.com>
2025-05-18 22:08:02 +0530

    Update Caeser_Cipher(Modified).py

--- Caeser_Cipher(Modified).py
+++ Caeser_Cipher(Modified).py
@@ -9,14 +9,14 @@ def decrypt(result,shift):
 print("---------------------Enter Your Choice -------------------\n1. Encrypt\n2. Decrypt\n3. Exit \n--------------------------------------")
 choice = (input("Enter your choice :- "))
 if choice == '1' :
-    num = input("Enter the Text u want to encrypt :- ")
+    num = input("Enter the Text u want to encrypt :-")
     shift = int(input("Enter the numeber of jump u want to (0-94) :-"))
-    print("Encrypt :- ",encrypt(num,shift))
+    print("Encrypt :-",encrypt(num,shift))
 elif choice == '2':
-    result = input("Enter the string u want to decrypt :- ")
+    result = input("Enter the string u want to decrypt :-")
     shift = int(input("Enter the numeber of jump u want to (0-94) :-"))
     print("Decrypt :-",decrypt(result,shift))  
 elif choice == '3':
     print("Thanks for using this program")
-    elif choice is not '1' or '2' or '3' :#This condition checks if the user has entered an invalid choice
+elif choice is not '1' or '2' or '3' :#This condition checks if the user has entered an invalid choice
     print("Invalid choice")
